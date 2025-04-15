769314386afe68ef7132c7fe516529763887b80b
CRMiteCodes <crmithun.shanjai@gmail.com>
2025-04-15 16:14:28 +0530

    WebServer_3

--- src/SimpleWebServer_3.java
+++ src/SimpleWebServer_3.java
@@ -3,15 +3,16 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class SimpleWebServer_2{
+public class SimpleWebServer_3{
     private static volatile boolean running = true;
     public static void main(String[] args) throws Exception{
-        ServerSocket server=new ServerSocket(8080);//wait for connection
-        System.out.println("Server is running on http://localhost:8080 enter 'exit' to stop server");
+        ServerSocket server = new ServerSocket(8080);
+        System.out.println("Multithreaded server running at http://localhost:8080 ");
+
         Thread consoleThread = new Thread(()->{
             Scanner scan=new Scanner(System.in);
             while (true){
-                String input=scan.nextLine();
+                String input = scan.nextLine();
                 if(input.equalsIgnoreCase("exit")){
                     System.out.println("Shutting down via console command...");
                     running = false;
@@ -27,17 +28,12 @@ public class SimpleWebServer_2{
             scan.close();
         });
         consoleThread.start();
+
         while(running){
             try{
                 Socket client = server.accept();
-                OutputStream out = client.getOutputStream();
-                String body = "<center><h1>Hello from Java Server 2!</h1></center>";
-                String response = "HTTP/1.1 200 OK\r\n"+"Content-Type: text/html\r\n"+"Content-Length: "+body.length() + "\r\n" + 
-                                    "Connection: close\r\n" + "\r\n" + body;                
-                out.write(response.getBytes());
-                out.flush();
-                out.close();
-                client.close();
+                System.out.println("New client connected");
+                new Thread(() -> handleClient(client)).start();
             }
             catch(Exception e){
                 if(running)
@@ -45,6 +41,22 @@ public class SimpleWebServer_2{
                 else
                 System.out.println("Server stopped");
             }
+        }
+    }
+
+    private static void handleClient(Socket client){
+        try{
+            OutputStream out = client.getOutputStream();
+            String body = "<h1> Hello from Multithreaded Server </h1>";
+            String response =  "HTTP/1.1 200 OK\r\n"+"Content-Type: text/html\r\n"+"Content-Length: "+body.length() + "\r\n" + 
+            "Connection: close\r\n" + "\r\n" + body;                
+            out.write(response.getBytes());
+            out.flush();
+            out.close();
+            client.close();
+        }
+        catch(Exception e){
+            System.out.println("Error handling client: "+e.getMessage());
         }
     }
 }
